3e4f328c57aae546db41766d016c9f63dd563b6e
ktanaha <moutoku1984@gmail.com>
2018-07-22 21:17:00 +0900

    add fibnatch

--- src/main/scala/example/ScalaTour.scala
+++ src/main/scala/example/ScalaTour.scala
@@ -2,7 +2,8 @@ package example
 
 object ScalaTour {
   def main(args: Array[String]): Unit = {
-    fizzBuzz(100)
+    //fizzBuzz(100)
+    println(fib(13))
   }
 
   def fizzBuzz(n: Int, i: Int = 1): Unit = {
@@ -18,5 +19,14 @@ object ScalaTour {
     }
 
     if (i < n) fizzBuzz(n, i + 1)
+  }
+
+  def fib(n: Int): Int = {
+    @scala.annotation.tailrec
+    def go(n: Int, prev: Int, curr: Int): Int =
+      if (n == 0) prev
+      else go (n - 1, curr, prev + curr)
+    
+    go(n, 0 ,1)
   }
 }
