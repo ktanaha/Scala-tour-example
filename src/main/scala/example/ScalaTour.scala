3682ad1f7c6b5eb1f3d2c87ffab3e3aff2da6978
ktanaha <moutoku1984@gmail.com>
2018-07-22 20:59:10 +0900

    update fizzbuzz match

--- src/main/scala/example/ScalaTour.scala
+++ src/main/scala/example/ScalaTour.scala
@@ -6,14 +6,15 @@ object ScalaTour {
   }
 
   def fizzBuzz(n: Int): Unit = for {i <- 1 to n} {
-    if (i % 15 == 0) {
-      println("FizzBuzz")
-    } else if (i % 5 == 0) {
-      println("Buzz")
-    } else if (i % 3 == 0) {
-      println("Fizz")
-    } else {
-      println(i)
+    i match {
+      case x if x % 15 == 0 =>
+        println("FizzBuzz")
+      case x if x % 5 == 0 =>
+        println("Buzz")
+      case x if x % 3 == 0 =>
+        println("Fizz")
+      case x =>
+        println(x)
     }
   }
 }
