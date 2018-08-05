2bb6e1942da37745826a3b518cef90ebf5a53722
ktanaha <moutoku1984@gmail.com>
2018-08-05 11:05:43 +0900

    FizzBuzz Update

--- src/main/scala/example/ScalaTour.scala
+++ src/main/scala/example/ScalaTour.scala
@@ -1,80 +1,49 @@
 package example
 
+import sbt.io.IO
+import java.io.File
+
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson._
+import org.json4s.jackson.Serialization.{read, write}
+
 object ScalaTour {
-
-  object Polygon {
-
-    def fromEdges(edges: List[Int]): Option[Polygon] =
-      edges.length match {
-        case 3 => Some(new Triangle(edges))
-        case x => None
-      }
-  }
-
-  abstract class Polygon(edges: List[Int]) {
-    val n = edges.size
-    val area: Double
-  }
-
-  class Triangle(edges: List[Int]) extends Polygon(edges) {
-    val a = edges(0)
-    val b = edges(1)
-    val c = edges(2)
-
-    val area = {
-      val s = (a + b + c) / 2.0
-      math.sqrt(s * (s - a) * (s - b) * (s - c))
-    }
-  }
+  implicit val formats = org.json4s.DefaultFormats
 
   def main(args: Array[String]): Unit = {
-    val invalidEdges2 = List(3, 4)
-    val invalidPolygon2 = Polygon.fromEdges(invalidEdges2)
+    val sourceFile = new File("sample.json")
+    val destinationFile = new File("fizzBuzz.json")
 
-    println(invalidPolygon2)
+    createSourceJSON(15, sourceFile)
 
-    val invalidEdges3 = List(3, 4, 5)
-    val invalidPolygon3 = Polygon.fromEdges(invalidEdges3)
-
-    println(invalidPolygon3)
+    fizzBuzzFromJSON(sourceFile, destinationFile)
   }
 
-}
+  def createSourceJSON(n: Int, srcFile: File): Unit = {
+    require(n >= 1)
 
-object FizzBuzz {
-
-  def toFizzBuzz(numbers: List[Int]): List[String] = {
-    val f: Int => String = (i: Int) => i match {
-      case x if x % 15 == 0 => "FizzBuzz"
-      case x if x % 3 == 0 => "Fizz"
-      case x if x % 5 == 0 => "Buzz"
-      case x => x.toString
-    }
-
-    numbers.map(f)
+    val intArrayHolder = IntArrayHolder((1 to n).toArray)
+    IO.write(srcFile, write(intArrayHolder))
   }
 
-  def fizzBuzz(n: Int, i: Int = 1): Unit = {
-    i match {
-      case x if x % 15 == 0 =>
-        println("FizzBuzz")
-      case x if x % 5 == 0 =>
-        println("Buzz")
-      case x if x % 3 == 0 =>
-        println("Fizz")
-      case x =>
-        println(x)
-    }
+  def fizzBuzzFromJSON(srcFile: File, dstFile: File): Unit = {
+    val rawJson = IO.read(srcFile)
+    val intArrayHolder = read[IntArrayHolder](rawJson)
 
-    if (i < n) fizzBuzz(n, i + 1)
-  }
+    val fizzBuzz = intArrayHolder.intArray.map(i =>
+      i match {
+        case x if x % 15 == 0 => "FizzBuzz"
+        case x if x % 5 == 0 => "Buzz"
+        case x if x % 3 == 0 => "Fizz"
+        case x => x.toString
+      })
 
-  def fib(n: Int): Int = {
-    @scala.annotation.tailrec
-    def go(n: Int, prev: Int, curr: Int): Int =
-      if (n == 0) prev
-      else go (n - 1, curr, prev + curr)
-    
-    go(n, 0 ,1)
+    val fizzBuzzHolder = FizzBuzzHolder(fizzBuzz)
+    IO.write(dstFile, write(fizzBuzzHolder))
   }
 }
+
+case class IntArrayHolder(intArray: Array[Int])
+case class FizzBuzzHolder(fizzBuzz: Array[String])
+
